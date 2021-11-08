c81152f0bfc24981053afe5341c524dc36026453
qor6 <87318054+qor6@users.noreply.github.com>
2021-11-09 01:23:26 +0900

    user들의 name, id, password, nickname을 그래프 데이터에 생성

--- app.py
+++ app.py
@@ -1,51 +1,47 @@
 from flask import Flask
 from neo4j import GraphDatabase
+import json
 
 app = Flask(__name__)
 
-appleComputer = ['iphone', 'ipad', 'ipod', 'mac']
 class connectDBMS :
     def __init__(self, url, user, password) :
         self.driver = GraphDatabase.driver(url, auth=(user, password))
 
-    def insert_apple(self):
+    def insert_users(self):
         with self.driver.session() as session:
-            greeting = session.write_transaction(self.INSERT_APPLEDATA)
+            greeting = session.write_transaction(self.INSERT_USERS_INFORMATION)
             return greeting
 
     def delete_apple(self):
         with self.driver.session() as session:
-            greeting = session.write_transaction(self.DELETE_APPLEDATA)
+            greeting = session.write_transaction(self.APPLY_USERS_INFORMATION)
             return greeting
 
-@staticmethod
-def INSERT_APPLEDATA(tx):
-    for i in range(0,4):
-       a = tx.run("CREATE (n:apple_product{name : $appleComputer})", appleComputer = appleComputer[i])
-    return 'apple computer is best'
 
     @staticmethod
-    def DELETE_APPLEDATA(tx):
+    def INSERT_USERS_INFORMATION(tx):
+        a = tx.run("CREATE (n:user{name : $username, id : $user_id, password : $user_password, nickname:$nickname})", username = "", user_id ="", user_password = "", nickname = "")
+        return '<h1>apple computer is best<h1>'
+
+    @staticmethod
+    def APPLY_USERS_INFORMATION(tx):
         a = tx.run("MATCH (n:apple_product)"
                    "DELETE n")
         return 'apple computer is worst'
 
-greeter = connectDBMS('bolt://localhost:7687','neo4j', '1234')
+greeter = connectDBMS('bolt://localhost:7687','neo4j', '0224')
 
 
 @app.route('/') # 기본페이지
-def main():  # put application's code here
+def home():  # put application's code here
     return 'Hello World!'
 
-@app.route('/setting') #
-def setting():  # put application's code here
-    a = greeter.insert_apple()
+@app.route('/Add_User') #
+def home_setting():  # put application's code here
+    a = greeter.insert_users()
     return a
 
-@app.route('/home') #
-def home():  # put application's code here
-    a = greeter.delete_apple()
-    return a
 
 if __name__ == '__main__':
-    app.run(host='localhost', port='5000', debug=True)
+    app.run(debug=True)
