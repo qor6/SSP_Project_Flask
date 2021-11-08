7ccaa613df3e0c4344bd36be6748c60d5b98f5f9
qor6 <87318054+qor6@users.noreply.github.com>
2021-11-08 20:33:44 +0900

    퍼온거 수정 중

--- app.py
+++ app.py
@@ -3,12 +3,49 @@ from neo4j import GraphDatabase
 
 app = Flask(__name__)
 
-class connectDBMS
+appleComputer = ['iphone', 'ipad', 'ipod', 'mac']
+class connectDBMS :
+    def __init__(self, url, user, password) :
+        self.driver = GraphDatabase.driver(url, auth=(user, password))
 
-@app.route('/')
-def hello_world():  # put application's code here
+    def insert_apple(self):
+        with self.driver.session() as session:
+            greeting = session.write_transaction(self.INSERT_APPLEDATA)
+            return greeting
+
+    def delete_apple(self):
+        with self.driver.session() as session:
+            greeting = session.write_transaction(self.DELETE_APPLEDATA)
+            return greeting
+
+@staticmethod
+def INSERT_APPLEDATA(tx):
+    for i in range(0,4):
+       a = tx.run("CREATE (n:apple_product{name : $appleComputer})", appleComputer = appleComputer[i])
+    return 'apple computer is best'
+
+    @staticmethod
+    def DELETE_APPLEDATA(tx):
+        a = tx.run("MATCH (n:apple_product)"
+                   "DELETE n")
+        return 'apple computer is worst'
+
+greeter = connectDBMS('bolt://localhost:7687','neo4j', '1234')
+
+
+@app.route('/') # 기본페이지
+def main():  # put application's code here
     return 'Hello World!'
 
+@app.route('/setting') #
+def setting():  # put application's code here
+    a = greeter.insert_apple()
+    return a
+
+@app.route('/home') #
+def home():  # put application's code here
+    a = greeter.delete_apple()
+    return a
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port='5000', debug=True)
