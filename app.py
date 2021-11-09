2f19f4b4985ac76c1446d96f811873ba60781c65
qor6 <87318054+qor6@users.noreply.github.com>
2021-11-09 15:25:34 +0900

    웹페이지 출력에 그래프데이터 정보 출력

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# neo4j의 brower에 start 와 open
+# 파이참의 재생 누르면 실행 가능
 from flask import Flask
 from neo4j import GraphDatabase
 import json
@@ -15,31 +17,37 @@ class connectDBMS :
 
     def delete_users(self):
         with self.driver.session() as session:
-            greeting = session.write_transaction(self.INSERT_USERS_INFORMATION)
+            greeting = session.write_transaction(self.DELETE_USERS_INFORMATION)
             return greeting
 
     @staticmethod
     def INSERT_USERS_INFORMATION(tx):
-        a = tx.run("CREATE (n:user{name : $username, id : $user_id, password : $user_password, nickname:$nickname})", username = "", user_id ="", user_password = "", nickname = "")
-        return 'apple computer is best'
+        a = tx.run("CREATE (n:user{name : $username, id : $user_id, password : $user_password, nickname:$nickname})",
+                   username = "", user_id ="", user_password = "", nickname = "")
+        return 'add user information success'
 
     @staticmethod
     def DELETE_USERS_INFORMATION(tx):
         a = tx.run("MATCH (n:user)""DELETE n")
-        return 'apple computer is worst'
+        return 'delete all user information success'
 
 greeter = connectDBMS('bolt://localhost:7687','neo4j', '0224')
 
 
 @app.route('/') # 기본페이지
 def home():  # put application's code here
-    return 'Hello World!'
+    return 'Hello!!'
 
-@app.route('/Add_User') #
-def home_setting():  # put application's code here
+@app.route('/user/add') #주소창에 /user/add로 입력하면
+def user_setting():  # put application's code here
     a = greeter.insert_users()
+    return a
+
+@app.route('/user/delete')
+def user_delete():  # put application's code here
+    a = greeter.delete_users()
     return a
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port='5000', debug=True)
