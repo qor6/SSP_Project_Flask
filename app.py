7fc8ff0657447cd03e66c84d059f977a6d813c83
qor6 <87318054+qor6@users.noreply.github.com>
2021-11-12 15:08:14 +0900

    오류 수정

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# neo4j의 brower에 start 와 open
+# neo4j start 와 open
 # 파이참의 재생 누르면 실행 가능
 from flask import Flask
 from neo4j import GraphDatabase
@@ -10,7 +10,7 @@ class connectDBMS :
     def __init__(self, url, user, password) :
         self.driver = GraphDatabase.driver(url, auth=(user, password))
 
-    def create_users(self):
+    def create_users(self): # 회원가입 관련
         with self.driver.session() as session:
             greeting = session.write_transaction(self.INSERT_USERS_INFORMATION)
             return greeting
@@ -20,15 +20,15 @@ class connectDBMS :
             greeting = session.write_transaction(self.DELETE_USERS_INFORMATION)
             return greeting
 
-    @staticmethod # (n:user:username)
+    @staticmethod # (u:user:username)
     def INSERT_USERS_INFORMATION(tx):
-        a = tx.run("CREATE (n:user{name : $username, id : $user_id, password : $user_password, nickname:$nickname})",
+        a = tx.run("CREATE (u:user{name : $username, id : $user_id, password : $user_password, nickname:$nickname})",
                    username = "", user_id ="", user_password = "", nickname = "")
         return 'add user information success'
 
     @staticmethod
     def DELETE_USERS_INFORMATION(tx):
-        a = tx.run("MATCH (n:user)""DELETE n")
+        z = tx.run("MATCH (u:user)""DELETE n")
         return 'delete all user information success'
 
 greeter = connectDBMS('bolt://localhost:7687','neo4j', '0224')
@@ -38,15 +38,18 @@ greeter = connectDBMS('bolt://localhost:7687','neo4j', '0224')
 def home():  # put application's code here
     return 'Hello!!'
 
-@app.route('/user/add') #주소창에 /user/add로 입력하면
-def user_add():  # put application's code here
+@app.route('/user/create') # 회원가입 페이지
+def user_add():
     a = greeter.create_users()
     return a
 
-@app.route('/user/delete')
-def user_delete():  # put application's code here
-    a = greeter.delete_users()
-    return a
+@app.route('/user/delete') # 모든 유저들 삭제
+def user_delete():
+    z = greeter.delete_users()
+    return z
 
 if __name__ == '__main__':
     app.run(host='localhost', port='5000', debug=True)
+
+# request
+# response
