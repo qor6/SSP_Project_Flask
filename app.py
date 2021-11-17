beb436a98151c96dfeee3f8fc15aae6ad19fc922
qor6 <87318054+qor6@users.noreply.github.com>
2021-11-17 21:27:29 +0900

    코드 추가 & 수정 필요함

--- app.py
+++ app.py
@@ -3,16 +3,26 @@
 from flask import Flask, jsonify, request, Response
 from neo4j import GraphDatabase
 
-
 app = Flask(__name__)
+
+def Login(inp_id, inp_pw, id, pw) :
+    if(inp_id == id & inp_pw == pw):
+        {
+            True
+        }
 
 class connectDBMS :
     def __init__(self, url, user, password) :
         self.driver = GraphDatabase.driver(url, auth=(user, password))
 
-    def create_users(self): # 회원가입 관련
+    def sign_users(self): # 회원가입 관련
         with self.driver.session() as session:
-            greeting = session.write_transaction(self.INSERT_USERS_INFORMATION)
+            greeting = session.write_transaction(self.SIGNUP_USERS_INFORMATION)
+            return greeting
+
+    def login_users(self): # 로그인 관련
+        with self.driver.session() as session:
+            greeting = session.write_transaction(self.LOGIN_USERS_INFORMATION)
             return greeting
 
     def delete_users(self): # 모든 유저 관련 정보 삭제
@@ -21,10 +31,17 @@ class connectDBMS :
             return greeting
 
     @staticmethod # (u:user:username)
-    def INSERT_USERS_INFORMATION(tx):
-        a = tx.run("CREATE (u:user{name : $username, id : $user_id, password : $user_password, nickname:$nickname})",
+    def SIGNUP_USERS_INFORMATION(tx):
+        a = tx.run("CREATE (u:user{name : $username, id : $user_id, pw : $user_password, nickname:$nickname})",
                    username = "", user_id ="", user_password = "", nickname = "")
         return 'add user information success'
+
+    @staticmethod
+    def LOGIN_USERS_INFORMATION(tx):
+        c = tx.run("MATCH (u:user{id : $user_id, pw : $user_password})"
+                   "RETURN u", user_id ="", user_password = "")
+        Login()
+        return 'Login success'
 
     @staticmethod
     def DELETE_USERS_INFORMATION(tx):
@@ -36,18 +53,58 @@ greeter = connectDBMS('bolt://localhost:7687','neo4j', '0224')
 
 @app.route('/') # 기본페이지
 def home():  # put application's code here
-    return 'Hello!!'
+    return jsonify(text='Hello, world')
 
-@app.route('/create') # 회원가입 페이지
-def user_add():
-    a = greeter.create_users()
+@app.route('/signup') # 회원가입 페이지
+def user_signup():
+    a = greeter.sign_users()
     return a
+"""
+@app.route('/login', methods=['POST']) # 로그인 페이지
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # 아래의 코드는 요청이 GET 이거나, 인증정보가 잘못됐을때 실행된다.
+    return
+"""
+
+@app.route('/login') # 로그인 페이지
+def user_login():
+    c = greeter.login_users()
+    return c
+"""
+@app.route('/userLogin', methods=['POST'])
+def userLogin():
+    user = request.get_json()  # json 데이터를 받아옴
+    return jsonify(user)  # 받아온 데이터를 다시 전송
 
 
-
-
-
-
+@app.route('/login/<userName>', methods=['POST']) # 로그인 페이지
+def user_Hello():
+    return 'hello, %s' #%(userName)
+"""
+# json 형태로 받아오는 함수 예시
+"""
+@app.route("/camera", methods=['POST'])
+def receive_cam():
+    global cam_msg
+    c_msg = request.get_json()
+    cam_msg = c_msg['flags']  #cam on : 0 | cam off : 1
+    print(type(cam_msg), cam_msg)
+    return 'received'
+    
+@app.route("/process", methods=['GET', 'POST'])
+def process():
+    content = request.json
+    result = {'result': True}
+    return jsonify(result)
+    
+"""
 
 @app.route('/user/delete') # 모든 유저들 삭제
 def user_delete():
