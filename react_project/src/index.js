f7bdde5f05b6aeca447c0cc72729c123659bd9cd
Prcnsi <ji.dozin@gmail.com>
2021-11-22 15:46:54 +0900

    Refactor the code structure

--- react_project/src/index.js
+++ react_project/src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './etc/reportWebVitals';
-import Login from './signUp/Login';
+import Login from './Login_Signup/Login';
+import SignUp from './Login_Signup/SignUp';
 
 // eslint-disable-next-line
 let element = ReactDOM.render(
   <React.StrictMode>
       <App />
       <Login/>
+      <SignUp/>
   </React.StrictMode>,
    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a Function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
